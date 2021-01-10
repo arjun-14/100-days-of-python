a6ac173f85cf616b03f07f61a7d1c6b92c174697
Arjun Narayanan <arjun14081999@gmail.com>
2021-01-10 15:19:57 +0530

    day 02: done

---
+++ day 02/bill_generator.py
@@ -0,0 +1,8 @@
+print('Welcome to the bill calculator.')
+totalBill = float(input('What was the total bill? $'))
+noOfPeople = float(input('How many people to split the bill? '))
+percentage = int(
+    input('What percentage tip would you like to give? 10, 12 or 15? '))
+amountToPay = round(
+    totalBill / noOfPeople + totalBill / noOfPeople * percentage / 100, 2)
+print(f'Each person should pay: ${amountToPay}')
