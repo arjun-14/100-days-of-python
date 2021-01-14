908e47f379e03df2e7f682f43c54d07d4298cefa
Arjun Narayanan <arjun14081999@gmail.com>
2021-01-14 20:05:05 +0530

    day 04: improvements

--- day 004/rock_paper_scissors.py
+++ day 004/rock_paper_scissors.py
@@ -1,21 +1,22 @@
 import random
 
-options = [
-    '''
+rock ='''
     _______
 ---'   ____)
       (_____)
       (_____)
       (____)
 ---.__(___)
-''', '''
+'''
+paper = '''
     _______
 ---'   ____)____
           ______)
           _______)
          _______)
 ---.__________)
-''', '''
+'''
+scissors = '''
     _______
 ---'   ____)____
           ______)
@@ -23,7 +24,8 @@ options = [
       (____)
 ---.__(___)
 '''
-]
+options = [rock, paper, scissors]
+
 win_condition = [(0,2),(1,0),(2,1)]
 
 user_choice = int(input('What do you choose ? Type 0 for rock, 1 for paper, 2 for scissors \n'))
@@ -33,7 +35,9 @@ print('Computer choose :')
 computer_choice = random.randint(0, 2)
 print(options[computer_choice])
 
-if(user_choice == computer_choice):
+if(user_choice > 2 or user_choice < 0):
+    print('Invalid choice')
+elif(user_choice == computer_choice):
   print('You draw')
 elif (user_choice, computer_choice) in win_condition:
   print('You win')
