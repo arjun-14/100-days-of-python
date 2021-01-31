526ccf6efb40061a5dd166a9298c2d7c8b8654d1
Arjun Narayanan <arjun14081999@gmail.com>
2021-01-31 23:03:34 +0530

    day 07: keeping track of user lives

--- day 007/hangman.py
+++ day 007/hangman.py
@@ -1,5 +1,62 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+lives = 6
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -9,11 +66,19 @@ display = ['_' for x in chosen_word]
 while(True):
     guess = input('Guess a letter : ').lower()
 
-    for letter in range(len(chosen_word)):
-        if chosen_word[letter] == guess:
-            display[letter] = guess
+    if guess in chosen_word:
+        for letter in range(len(chosen_word)):
+            if chosen_word[letter] == guess:
+                display[letter] = guess
+    else:
+        lives -=1
+        print(stages[lives])
+        if(lives == 0):
+            print('You lose')
+            break
         
-    print(display)
+        
+    print(' '.join(display))
     
     if ('_' not in display):
         print('You win')
