cfbbfd764b32557115f6b237e3c1a1023692f229
Arjun Narayanan <arjun14081999@gmail.com>
2021-01-31 21:12:41 +0530

    day 07: checking if player has won

--- day 007/hangman.py
+++ day 007/hangman.py
@@ -1,16 +1,20 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 
-
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
 display = ['_' for x in chosen_word]
 
-guess = input('Guess a letter : ').lower()
+while(True):
+    guess = input('Guess a letter : ').lower()
 
-for letter in range(len(chosen_word)):
-    if chosen_word[letter] == guess:
-        display[letter] = guess
-       
-print(display)
+    for letter in range(len(chosen_word)):
+        if chosen_word[letter] == guess:
+            display[letter] = guess
+        
+    print(display)
+    
+    if ('_' not in display):
+        print('You win')
+        break
