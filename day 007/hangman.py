605d6fb6d854065b086c77c01c41b14338741819
Arjun Narayanan <arjun14081999@gmail.com>
2021-01-31 23:32:41 +0530

    day 07: improving ux

--- day 007/hangman.py
+++ day 007/hangman.py
@@ -1,85 +1,41 @@
 import random
-word_list = ["aardvark", "baboon", "camel"]
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+import hangman_art as art
+import hangman_words as words
+
+word_list = words.word_list
+stages = art.stages
 lives = 6
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
 display = ['_' for x in chosen_word]
+guessed_letters = []
+print(art.logo)
 
 while(True):
     guess = input('Guess a letter : ').lower()
 
+    if guess in guessed_letters:
+        print('You have already guessed the letter '+ guess)
+        continue
+    else:
+        guessed_letters.append(guess)
+    
     if guess in chosen_word:
         for letter in range(len(chosen_word)):
             if chosen_word[letter] == guess:
                 display[letter] = guess
+        
     else:
         lives -=1
-        print(stages[lives])
+        print('You guessed '+guess+', that\'s not in the word. You lose a life.')
         if(lives == 0):
             print('You lose')
             break
-        
-        
     print(' '.join(display))
-    
+    print(stages[lives])
+            
     if ('_' not in display):
         print('You win')
         break
