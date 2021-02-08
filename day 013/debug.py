5b8989ec9f203ec0f45fe1960f9ce5b5f27e656a
Arjun Narayanan <arjun14081999@gmail.com>
2021-02-08 21:56:48 +0530

    day 13: done

---
+++ day 013/debug.py
@@ -0,0 +1,87 @@
+############DEBUGGING#####################
+
+# # Describe Problem
+# def my_function():
+#   for i in range(1, 20):
+#     if i == 20:
+#       print("You got it")
+# my_function()
+
+#i range from 1 to 19, doesnt include 20.
+def my_function():
+  for i in range(1, 21):
+    if i == 20:
+      print("You got it")
+my_function()
+
+# # Reproduce the Bug
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dice_num = randint(1, 6)
+# print(dice_imgs[dice_num])
+
+#randint would return number between 1-6, including 1 and 6.
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5 )
+print(dice_imgs[dice_num])
+
+# # Play Computer
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year > 1994:
+#   print("You are a Gen Z.")
+
+# no output for 1994.
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year >= 1994:
+  print("You are a Gen Z.")
+
+# # Fix the Errors
+# age = input("How old are you?")
+# if age > 18:
+# print("You can drive at age {age}.")
+
+# indentaion . f string . input is string, should be casted to int
+age = int(input("How old are you?"))
+if age > 18:
+    print(f"You can drive at age {age}.")
+
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+# double equals in word_per_change = checking expressions, not assignment
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
+
+# #Use a Debugger
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#   b_list.append(new_item)
+#   print(b_list)
+
+# mutate([1,2,3,5,8,13])
+
+#append after loop
+def mutate(a_list):
+  b_list = []
+  for item in a_list:
+    new_item = item * 2
+    b_list.append(new_item)
+  print(b_list)
+
+mutate([1,2,3,5,8,13])
